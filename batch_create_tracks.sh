e3048158a99dbbba8747ce24a9d6b4ed47becf21
Allie Tran <allie@lone.computing.dcu.ie>
2026-01-06 16:10:38 +0000

    Change batch script

--- batch_create_tracks.sh
+++ batch_create_tracks.sh
@@ -1,8 +1,11 @@
-for day in day2 day1 day3 day4
+# for day in day2 day1 day3 day4
+for day in day1
 do
-    for people in Allie Bjorn Cathal Florian Klaus Luca Onanong Stevan Tien Werner
+    # for people in Allie Bjorn Cathal Florian Klaus Luca Onanong Stevan Tien Werner
+    for people in Bjorn Cathal Florian Klaus Luca Onanong Stevan Tien Werner
     # for people in Kitchen Living1 Living2 Meeting
     do
-        python3 track_and_describe.py --person "$people" --day "$day"
+        # python3 track_and_describe.py --person "$people" --day "$day"
+        python track_and_describe.py --person "$people" --day "$day" --input_folder ~/castle/processed --output_folder ~/castle/people-tracks --no_descriptions
     done
 done
